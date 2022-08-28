41888b69692941062d9e7603bb435a4438228ad3
Kwan Wu <kwan@letslinc.com>
2022-08-28 17:27:20 +0800

    0828

--- nvim/install.sh
+++ nvim/install.sh
@@ -1,10 +1,10 @@
 #vimrc setup
-ln -s ~/.dotfiles/nvim/vim.symlink ~/.vimrc
+ln -s ~/.dotfiles/nvim/nvim.symlink ~/.vimrc
 mkdir -p ~/.config/nvim
 ln -s ~/.vimrc ~/.config/nvim/init.vim
 
 #Vundle setup
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.config/nvim/init.vim
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.config/nvim/bundle/Vundle.vim
 pip install pynvim
 .~/.config/nvim/bundle/YouCompleteMe/install.py
 vim +PlugInstall
