e594a914e77c8fe73a9db8d7b05e0a75f445338a
ckjeter <ckjeter7@gmail.com>
2022-08-28 18:46:20 +0800

    Bug fix

--- vim/install.sh
+++
@@ -1,5 +0,0 @@
-ln -s ~/.dotfiles/vim/vim.symlink ~/.vimrc
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-vim +PlugInstall
-#python3 ~/.vim/plugged/YouCompleteMe/install.py --all > /dev/null 2>&1
-
