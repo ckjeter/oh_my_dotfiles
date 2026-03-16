7170cd29c53c8a2920df0f498b35427f5c7d99e3
Kuan Wu <33897307+ckjeter@users.noreply.github.com>
2026-03-16 18:39:50 -0400

    feat(ghostty): add catppuccin mocha theme and install script (#13)
    * feat(ghostty): add catppuccin mocha theme and install script
    
    * chore(ghostty): add default config template as reference
    
    * feat(ghostty): add catppuccin theme files and symlink via install script

---
+++ ghostty/install.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+mkdir -p ~/.config/ghostty/themes
+ln -sf ~/.dotfiles/ghostty/config ~/.config/ghostty/config
+ln -sf ~/.dotfiles/ghostty/themes/catppuccin-mocha.conf ~/.config/ghostty/themes/catppuccin-mocha.conf
+ln -sf ~/.dotfiles/ghostty/themes/catppuccin-macchiato.conf ~/.config/ghostty/themes/catppuccin-macchiato.conf
